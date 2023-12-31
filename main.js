cac30ab992f02fb87788b6b5b3db8d37452b740f
Bruno <brunoamarante83@gmail.com>
2023-12-31 09:40:57 -0300

    commit inicial

---
+++ main.js
@@ -0,0 +1,72 @@
+
+AOS.init();
+
+var swiper = new Swiper(".mySwiper", {
+    loop: true,
+    autoplay: true,
+    speed: 2000,
+    slidesPerView: 2,
+    spaceBetween: 30,
+    centeredSlides: false,
+    pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+
+var swiper = new Swiper(".produtos", {
+    loop: true,
+    autoplay: true,
+    speed: 2000,
+    slidesPerView: 4,
+    spaceBetween: 47,
+    centeredSlides: true,
+    pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+
+//------------------Carrosel MOBILE-------------------//
+var swiper = new Swiper(".carrosel-mobile", {
+    loop: true,
+    autoplay: true,
+    speed: 2000,
+    slidesPerView: 1,
+    spaceBetween: 30,
+    centeredSlides: false,
+    pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+
+var swiper = new Swiper(".mobile-produtos", {
+    loop: true,
+    autoplay: true,
+    speed: 2000,
+    slidesPerView: 1,
+    spaceBetween: 20,
+    centeredSlides: true,
+    pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+
